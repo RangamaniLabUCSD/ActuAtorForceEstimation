0d0b93986e480784a6f2edbe229df268ae5b9b02
Christopher T. Lee <ctlee@ucsd.edu>
2022-08-08 11:50:55 -0700

    Create material class; Add integrator func
    Signed-off-by: Christopher T. Lee <ctlee@ucsd.edu>

--- automembrane/energy.py
+++ automembrane/energy.py
@@ -5,6 +5,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 
+from abc import ABC, abstractmethod
 from functools import partial
 
 import jax
@@ -12,152 +13,121 @@ import jax.numpy as jnp
 import numpy as np
 import numpy.typing as npt
 
-from . import util as u
+# @partial(jax.jit, static_argnames=["Kb", "Ksg", "Ksl"])
+# def _get_energy_2d_closed(
+#     vertex_positions: npt.NDArray[np.float64],
+#     Kb: float = 0.1,
+#     Ksg: float = 50,
+#     Ksl: float = 1,
+# ) -> npt.NDArray[np.float64]:
+#     """Compute the energy of a 2D discrete closed polygon.
+
+#     Note that this function assumes that the coordinates of the last point are the same as the first point.
+
+#     Args:
+#         vertex_positions (npt.NDArray[np.float64]): Coordinates
+#         Kb (float, optional): Bending modulus in units of pN um.Defaults to 1.
+#         Ksg (float, optional): Global stretching modulus in units of PN um/um^2. Defaults to 0.
+
+#     Returns:
+#         npt.NDArray[np.float64]: Componentwise energies of the system
+#     """
+#     d_pos = jnp.roll(vertex_positions[:-1], -1, axis=0) - vertex_positions[:-1]
+#     edgeLengths = jnp.linalg.norm(d_pos, axis=1)
+#     referenceEdgeLength = jnp.mean(edgeLengths)
+
+#     edgeAbsoluteAngles = jnp.arctan2(d_pos[:, 1], d_pos[:, 0])
+
+#     vertexTurningAngles = (jnp.roll(edgeAbsoluteAngles, -1) - edgeAbsoluteAngles) % (
+#         2 * jnp.pi
+#     )
+#     vertexTurningAngles = (vertexTurningAngles + jnp.pi) % (2 * jnp.pi) - jnp.pi
+
+#     tan_vertex_turning_angles = jnp.tan(vertexTurningAngles / 2)
+
+#     edgeCurvatures = (
+#         tan_vertex_turning_angles + jnp.roll(tan_vertex_turning_angles, 1)
+#     ) / edgeLengths
+
+#     bendingEnergy = Kb * jnp.sum(edgeCurvatures * edgeCurvatures * edgeLengths)
+#     surfaceEnergy = Ksg * jnp.sum(edgeLengths)
+#     regularizationEnergy = Ksl * jnp.sum(
+#         ((edgeLengths - referenceEdgeLength) / referenceEdgeLength) ** 2
+#     )
+#     return jnp.array([bendingEnergy, surfaceEnergy, regularizationEnergy])
 
 
-@partial(jax.jit, static_argnames=["Kb", "Ksg"])
-def _get_energy_2d_closed(
-    vertex_positions: npt.NDArray[np.float64],
-    Kb: float = 0.1,
-    Ksg: float = 50,
-    Ksl: float = 1,
-) -> npt.NDArray[np.float64]:
-    """Compute the energy of a 2D discrete closed polygon.
+# def get_energy_2d_closed(
+#     vertex_positions: npt.NDArray[np.float64],
+#     Kb: float = 0.1,
+#     Ksg: float = 50,
+#     Ksl: float = 1,
+# ) -> npt.NDArray[np.float64]:
+#     """Compute the energy of a 2D discrete closed polygon.
 
-    Note that this function assumes that the coordinates of the last point are the same as the first point.
+#     Note that this function assumes that the coordinates of the last point are the same as the first point.
 
-    Args:
-        vertex_positions (npt.NDArray[np.float64]): Coordinates
-        Kb (float, optional): Bending modulus in units of pN um.Defaults to 1.
-        Ksg (float, optional): Global stretching modulus in units of PN um/um^2. Defaults to 0.
+#     Args:
+#         vertex_positions (npt.NDArray[np.float64]): Coordinates
+#         Kb (float, optional): Bending modulus in units of pN um.Defaults to 1.
+#         Ksg (float, optional): Global stretching modulus in units of PN um/um^2. Defaults to 0.
 
-    Returns:
-        npt.NDArray[np.float64]: Componentwise energies of the system
-    """
-    d_pos = jnp.roll(vertex_positions[:-1], -1, axis=0) - vertex_positions[:-1]
-    edgeLengths = jnp.linalg.norm(d_pos, axis=1)
-    referenceEdgeLength = jnp.mean(edgeLengths)
-
-    edgeAbsoluteAngles = jnp.arctan2(d_pos[:, 1], d_pos[:, 0])
-
-    vertexTurningAngles = (jnp.roll(edgeAbsoluteAngles, -1) - edgeAbsoluteAngles) % (
-        2 * jnp.pi
-    )
-    vertexTurningAngles = (vertexTurningAngles + jnp.pi) % (2 * jnp.pi) - jnp.pi
-
-    tan_vertex_turning_angles = jnp.tan(vertexTurningAngles / 2)
-
-    edgeCurvatures = (
-        tan_vertex_turning_angles + jnp.roll(tan_vertex_turning_angles, 1)
-    ) / edgeLengths
-
-    bendingEnergy = Kb * jnp.sum(edgeCurvatures * edgeCurvatures * edgeLengths)
-    surfaceEnergy = Ksg * jnp.sum(edgeLengths)
-    regularizationEnergy = Ksl * jnp.sum(
-        ((edgeLengths - referenceEdgeLength) / referenceEdgeLength) ** 2
-    )
-    return bendingEnergy, surfaceEnergy, regularizationEnergy
+#     Returns:
+#         float: Energy of the system
+#     """
+#     if not jnp.allclose(vertex_positions[-1], vertex_positions[0]):
+#         raise RuntimeError(
+#             f"First ({vertex_positions[0]}) and last ({vertex_positions[-1]}) points are expected to be the same."
+#         )
+#     return _get_energy_2d_closed(vertex_positions, Kb, Ksg, Ksl)
 
 
-def get_energy_2d_closed(
-    vertex_positions: npt.NDArray[np.float64],
-    Kb: float = 0.1,
-    Ksg: float = 50,
-    Ksl: float = 1,
-) -> float:
-    """Compute the energy of a 2D discrete closed polygon.
-
-    Note that this function assumes that the coordinates of the last point are the same as the first point.
-
-    Args:
-        vertex_positions (npt.NDArray[np.float64]): Coordinates
-        Kb (float, optional): Bending modulus in units of pN um.Defaults to 1.
-        Ksg (float, optional): Global stretching modulus in units of PN um/um^2. Defaults to 0.
-
-    Returns:
-        float: Energy of the system
-    """
-    if not jnp.allclose(vertex_positions[-1], vertex_positions[0]):
-        raise RuntimeError(
-            f"First ({vertex_positions[0]}) and last ({vertex_positions[-1]}) points are expected to be the same."
-        )
-    return _get_energy_2d_closed(vertex_positions, Kb, Ksg)
+# @partial(jax.jit, static_argnames=["Kb", "Ksg"])
+# def _get_force_2d_closed(
+#     vertex_positions: npt.NDArray[np.float64],
+#     Kb: float = 0.1,
+#     Ksg: float = 50,
+# ) -> npt.NDArray[np.float64]:
+#     f_energy = partial(_get_energy_2d_closed, Kb=Kb, Ksg=Ksg)
+#     # return jax.grad(f_energy)(vertex_positions)
+#     return jax.jacrev(f_energy)(vertex_positions)
 
 
-@partial(jax.jit, static_argnames=["Kb", "Ksg"])
-def _get_force_2d_closed(
-    vertex_positions: npt.NDArray[np.float64],
-    Kb: float = 0.1,
-    Ksg: float = 50,
-):
-    f_energy = partial(_get_energy_2d_closed, Kb=Kb, Ksg=Ksg)
-    # return jax.grad(f_energy)(vertex_positions)
-    return jax.jacrev(f_energy)(vertex_positions)
+# def get_force_2d_closed(
+#     vertex_positions: npt.NDArray[np.float64],
+#     Kb: float = 0.1,
+#     Ksg: float = 50,
+# ) -> npt.NDArray[np.float64]:
+#     """Compute the force of a 2D discrete closed polygon.
+
+#     Note that this function assumes that the coordinates of the last point are the same as the first point.
+
+#     Args:
+#         vertex_positions (npt.NDArray[np.float64]): Coordinates
+#         Kb (float, optional): Bending modulus in units of pN um.Defaults to 1.
+#         Ksg (float, optional): Global stretching modulus in units of PN um/um^2. Defaults to 0.
+
+#     Returns:
+#         float: Energy of the system
+#     """
+#     if not jnp.allclose(vertex_positions[-1], vertex_positions[0]):
+#         raise RuntimeError(
+#             f"First ({vertex_positions[0]}) and last ({vertex_positions[-1]}) points are expected to be the same."
+#         )
+#     return _get_force_2d_closed(vertex_positions, Kb, Ksg)
 
 
-def get_force_2d_closed(
-    vertex_positions: npt.NDArray[np.float64],
-    Kb: float = 0.1,
-    Ksg: float = 50,
-):
-    """Compute the force of a 2D discrete closed polygon.
-
-    Note that this function assumes that the coordinates of the last point are the same as the first point.
-
-    Args:
-        vertex_positions (npt.NDArray[np.float64]): Coordinates
-        Kb (float, optional): Bending modulus in units of pN um.Defaults to 1.
-        Ksg (float, optional): Global stretching modulus in units of PN um/um^2. Defaults to 0.
-
-    Returns:
-        float: Energy of the system
-    """
-    if not jnp.allclose(vertex_positions[-1], vertex_positions[0]):
-        raise RuntimeError(
-            f"First ({vertex_positions[0]}) and last ({vertex_positions[-1]}) points are expected to be the same."
-        )
-    return _get_force_2d_closed(vertex_positions, Kb, Ksg)
-
-
-def get_energy_2d_closed_notrace(
-    vertex_positions: npt.NDArray[np.float64],
-    Kb: float = 1,
-    Ksg: float = 0,
-) -> tuple[float, float]:
-    """Compute the energy of a 2D discrete closed polygon without jax tracing.
-
-    Args:
-        vertex_positions (npt.NDArray[np.float64]): Coordinates
-        Kb (float, optional): Bending modulus. Defaults to 1.
-        Ksg (float, optional): Global stretching modulus. Defaults to 0.
-
-    Returns:
-        tuple(float, float): Tuple of bending energy and surface energy
-    """
-    if not np.allclose(vertex_positions[-1], vertex_positions[0]):
-        raise RuntimeError(
-            f"First ({vertex_positions[0]}) and last ({vertex_positions[-1]}) points are expected to be the same."
-        )
-
-    d_pos = np.roll(vertex_positions[:-1], -1, axis=0) - vertex_positions[:-1]
-    edgeLengths = np.linalg.norm(d_pos, axis=1)
-    edgeAbsoluteAngles = np.arctan2(d_pos[:, 1], d_pos[:, 0])
-
-    vertexTurningAngles = (np.roll(edgeAbsoluteAngles, -1) - edgeAbsoluteAngles) % (
-        2 * np.pi
-    )
-    vertexTurningAngles = (vertexTurningAngles + np.pi) % (2 * np.pi) - np.pi
-
-    tan_vertex_turning_angles = np.tan(vertexTurningAngles / 2)
-
-    edgeCurvatures = (
-        tan_vertex_turning_angles + np.roll(tan_vertex_turning_angles, 1)
-    ) / edgeLengths
-
-    bendingEnergy = Kb * np.sum(edgeCurvatures * edgeCurvatures * edgeLengths)
-    surfaceEnergy = Ksg * np.sum(edgeLengths)
-
-    return bendingEnergy, surfaceEnergy
+# @partial(jax.jit, static_argnames=["Kb", "Ksg", "Ksl"])
+# def _get_energy_force_2d_closed(
+#     self,
+#     vertex_positions: npt.NDArray[np.float64],
+#     Kb: float = 0.1,
+#     Ksg: float = 50,
+#     Ksl: float = 1,
+# ) -> tuple[npt.NDArray[np.float64], npt.NDArray[np.float64]]:
+#     energy, vjp = jax.vjp(_get_energy_2d_closed, vertex_positions, Kb, Ksg, Ksl)
+#     force = jax.vmap(vjp, in_axes=0)(jnp.ones_like(energy))
+#     return energy, force
 
 
 @partial(jax.jit, static_argnames=["Kb", "Ksg"])
@@ -199,39 +169,160 @@ def get_energy_2d_open(
     return bendingEnergy + surfaceEnergy
 
 
-def get_energy_2d_open_notrace(
-    vertex_positions: npt.NDArray[np.float64],
-    Kb: float = 0.1,
-    Ksg: float = 50,
-) -> float:
-    """Compute the energy of a 2D discrete open polygon without jax tracing
+class Material(ABC):
+    @abstractmethod
+    def energy(
+        self, vertex_positions: npt.NDArray[np.float64]
+    ) -> npt.NDArray[np.float64]:
+        pass
 
-    Args:
-        vertex_positions (npt.NDArray[np.float64]): Coordinates
-        Kb (float, optional): Bending modulus. Defaults to 1.
-        Ksg (float, optional): Global stretching modulus. Defaults to 0.
+    @abstractmethod
+    def force(
+        self, vertex_positions: npt.NDArray[np.float64]
+    ) -> npt.NDArray[np.float64]:
+        pass
 
-    Returns:
-         tuple(float, float): Tuple of bending energy and surface energy
-    """
-    x = vertex_positions[:, 0]
-    y = vertex_positions[:, 1]
-    dx = np.diff(x)
-    dy = np.diff(y)
-    edgeLengths = np.sqrt(dx**2 + dy**2)
-    edgeAbsoluteAngles = np.arctan2(dy, dx)
+    @abstractmethod
+    def energy_force(
+        self, vertex_positions: npt.NDArray[np.float64]
+    ) -> npt.NDArray[np.float64]:
+        pass
 
-    vertexTurningAngles = (np.diff(edgeAbsoluteAngles)) % (2 * np.pi)
-    vertexTurningAngles = (vertexTurningAngles + np.pi) % (2 * np.pi) - np.pi
 
-    vertexTurningAngles = np.append(vertexTurningAngles, vertexTurningAngles[-1])
-    vertexTurningAngles = np.append(vertexTurningAngles[0], vertexTurningAngles)
+class ClosedPlaneCurveMaterial(Material):
+    def __init__(
+        self,
+        Kb: float = 0.1,
+        Ksg: float = 50,
+        Ksl: float = 1,
+    ):
+        """_summary_
 
-    edgeCurvatures = (
-        np.tan(vertexTurningAngles[:-1] / 2) + np.tan(vertexTurningAngles[1:] / 2)
-    ) / edgeLengths
+        Args:
+            Kb (float, optional): Bending modulus in units of pN um.Defaults to 1.
+            Ksg (float, optional): Global stretching modulus in units of PN um/um^2. Defaults to 0.
+            Ksl (float, optional): Regularization modulus. Defaults to 1.
+        """
+        self.Kb = Kb
+        self.Ksg = Ksg
+        self.Ksl = Ksl
 
-    bendingEnergy = Kb * np.sum(edgeCurvatures * edgeCurvatures * edgeLengths)
-    surfaceEnergy = Ksg * np.sum(edgeLengths)
+    def _check_valid(
+        self,
+        vertex_positions: npt.NDArray[np.float64],
+    ) -> bool:
+        """_summary_
 
-    return bendingEnergy, surfaceEnergy
+        Args:
+            vertex_positions (npt.NDArray[np.float64]): _description_
+
+        Raises:
+            RuntimeError: _description_
+
+        Returns:
+            bool: _description_
+        """
+        if not np.allclose(vertex_positions[-1], vertex_positions[0]):
+            raise RuntimeError(
+                f"First ({vertex_positions[0]}) and last ({vertex_positions[-1]}) points are expected to be the same."
+            )
+        return True
+
+    @partial(jax.jit, static_argnums=0)
+    def _energy(
+        self,
+        vertex_positions: npt.NDArray[np.float64],
+    ) -> npt.NDArray[np.float64]:
+        """Compute the energy of a 2D discrete closed polygon.
+
+        Note that this function assumes that the coordinates of the last point are the same as the first point.
+
+        Args:
+            vertex_positions (npt.NDArray[np.float64]): Coordinates
+
+        Returns:
+            npt.NDArray[np.float64]: Componentwise energies of the system
+        """
+        d_pos = jnp.roll(vertex_positions[:-1], -1, axis=0) - vertex_positions[:-1]
+        edgeLengths = jnp.linalg.norm(d_pos, axis=1)
+        referenceEdgeLength = jnp.mean(edgeLengths)
+
+        edgeAbsoluteAngles = jnp.arctan2(d_pos[:, 1], d_pos[:, 0])
+
+        vertexTurningAngles = (
+            jnp.roll(edgeAbsoluteAngles, -1) - edgeAbsoluteAngles
+        ) % (2 * jnp.pi)
+        vertexTurningAngles = (vertexTurningAngles + jnp.pi) % (2 * jnp.pi) - jnp.pi
+
+        tan_vertex_turning_angles = jnp.tan(vertexTurningAngles / 2)
+
+        edgeCurvatures = (
+            tan_vertex_turning_angles + jnp.roll(tan_vertex_turning_angles, 1)
+        ) / edgeLengths
+
+        bendingEnergy = self.Kb * jnp.sum(edgeCurvatures * edgeCurvatures * edgeLengths)
+        surfaceEnergy = self.Ksg * jnp.sum(edgeLengths)
+        regularizationEnergy = self.Ksl * jnp.sum(
+            ((edgeLengths - referenceEdgeLength) / referenceEdgeLength) ** 2
+        )
+        return jnp.array([bendingEnergy, surfaceEnergy, regularizationEnergy])
+
+    def energy(
+        self, vertex_positions: npt.NDArray[np.float64]
+    ) -> npt.NDArray[np.float64]:
+        """Compute the energy of a 2D discrete closed polygon.
+
+        Note that this function assumes that the coordinates of the last point are the same as the first point.
+
+        Args:
+            vertex_positions (npt.NDArray[np.float64]): Coordinates
+            Kb (float, optional): Bending modulus in units of pN um.Defaults to 1.
+            Ksg (float, optional): Global stretching modulus in units of PN um/um^2. Defaults to 0.
+
+        Returns:
+            float: Energy of the system
+        """
+        self._check_valid(vertex_positions)
+        return self._energy(vertex_positions)
+
+    @partial(jax.jit, static_argnums=0)
+    def _force(
+        self,
+        vertex_positions: npt.NDArray[np.float64],
+    ) -> npt.NDArray[np.float64]:
+        # return jax.grad(f_energy)(vertex_positions)
+        return jax.jacrev(self._energy)(vertex_positions)
+
+    def force(
+        self,
+        vertex_positions: npt.NDArray[np.float64],
+    ) -> npt.NDArray[np.float64]:
+        """Compute the force of a 2D discrete closed polygon.
+
+        Note that this function assumes that the coordinates of the last point are the same as the first point.
+
+        Args:
+            vertex_positions (npt.NDArray[np.float64]): Coordinates
+            Kb (float, optional): Bending modulus in units of pN um.Defaults to 1.
+            Ksg (float, optional): Global stretching modulus in units of PN um/um^2. Defaults to 0.
+
+        Returns:
+            float: Energy of the system
+        """
+        self._check_valid(vertex_positions)
+        return self._force(vertex_positions)
+
+    @partial(jax.jit, static_argnums=0)
+    def _energy_force(
+        self,
+        vertex_positions: npt.NDArray[np.float64],
+    ) -> tuple[npt.NDArray[np.float64], npt.NDArray[np.float64]]:
+        energy, vjp = jax.vjp(self._energy, vertex_positions)
+        (force,) = jax.vmap(vjp, in_axes=0)(jnp.eye(len(energy)))
+        return energy, force
+
+    def energy_force(
+        self, vertex_positions: npt.NDArray[np.float64]
+    ) -> tuple[npt.NDArray[np.float64], npt.NDArray[np.float64]]:
+        self._check_valid(vertex_positions)
+        return self._energy_force(vertex_positions)
--- automembrane/integrator.py
+++ automembrane/integrator.py
@@ -4,8 +4,42 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+from functools import partial
+import jax
 
-def fwd_euler_integrator(n_steps: int = 1e5, dt: float = 5e-6):
+import numpy as np
+import numpy.typing as npt
+from tqdm.auto import tqdm
 
-    
-    return 1
+from .energy import Material
+
+
+# @partial(jax.jit, static_argnums=[1], static_argnames=["n_steps", "dt"])
+def fwd_euler_integrator(
+    coords: npt.NDArray[np.float64],
+    mat: Material,
+    n_steps: int = int(1e5),
+    dt: float = 5e-6,
+) -> tuple[npt.NDArray[np.float64], npt.NDArray[np.float64]]:
+    """Perform forward Euler integration
+
+    Args:
+        coords (npt.NDArray[np.float64]): Vertex coordinate position
+        mat (Material): Membrane material holding parameters
+        n_steps (int, optional): Number of steps to take. Defaults to int(1e5).
+        dt (float, optional): Time step. Defaults to 5e-6.
+
+    Returns:
+        tuple[npt.NDArray[np.float64], npt.NDArray[np.float64]]: _description_
+    """
+    energy_log = np.zeros((n_steps, *mat._energy(coords).shape))
+
+    for i in tqdm(range(0, n_steps), desc="Energy relaxation"):
+        energy_log[i], force = mat._energy_force(coords)
+        # c_t+1 = c_t - force * dt
+        coords = np.array(coords - np.sum(force, axis=0) * dt)
+        coords[-1] = coords[0]
+    # print(
+    #     f"  DELTA E: {energy_log[-1] - energy_log[0]}; E_before: {energy_log[0]}; E_after: {energy_log[-1]}"
+    # )
+    return coords, energy_log
