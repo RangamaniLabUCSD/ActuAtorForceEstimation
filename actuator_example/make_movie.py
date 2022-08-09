61be3eab71888de8deb581c05061ec7595825ef4
Christopher T. Lee <ctlee@ucsd.edu>
2022-08-08 17:18:22 -0700

    Add integrator; improve movie script
    Signed-off-by: Christopher T. Lee <ctlee@ucsd.edu>

--- actuator_example/make_movie.py
+++ actuator_example/make_movie.py
@@ -1,93 +1,53 @@
-# Copyright (c) 2022 Eleanor Jung, Cuncheng Zhu, and Christopher T. Lee
+# Copyright (c) 2022 Eleanor Jung, Cuncheng Zhu, and Christopher T. lee
 #
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-
 import math
-import tempfile
-from collections import defaultdict
+
 from functools import partial
-from pathlib import Path
 
 import matplotlib as mpl
+import matplotlib.animation as animation
 import matplotlib.pyplot as plt
-import moviepy.editor as mpy
 import numpy as np
-
-# from jax import jit
-# from PIL import Image
+import jax
+from PIL import Image
 from scipy.interpolate import splev, splprep
+from tqdm.auto import tqdm
 from tqdm.contrib.concurrent import process_map
-from tqdm import tqdm
 
 import automembrane.util as u
-from automembrane.energy import *
+from automembrane.energy import ClosedPlaneCurveMaterial
+from automembrane.integrator import fwd_euler_integrator
 
+from actuator_constants import files, images
 
+jax.config.update("jax_enable_x64", True)
 u.matplotlibStyle(small=10, medium=12, large=14)
 
-# List of segmentations as Paths
-files = list(
-    map(
-        Path,
-        [
-            f"coordinates/{i}"
-            for i in [
-                "cell1/34D-grid2-s3-acta1_001_16.txt",
-                "cell2/34D-grid3-ActA1_007_16.txt",
-                "cell2/34D-grid3-ActA1_013_16.txt",
-                "cell3/34D-grid2-s2_002_16.txt",
-                "cell3/34D-grid2-s5_005_16.txt",
-                "cell3/34D-grid3-ActA1_020_16.txt",
-                "cell3/34D-grid3-s6_005_16.txt",
-                "cell4/34D-grid2-s3_028_16.txt",
-                "cell5/34D-grid3-ActA1_001_16.txt",
-                "cell5/34D-grid3-ActA1_002_16.txt",
-                "cell5/34D-grid3-ActA1_003_16.txt",
-                "cell5/34D-grid3-ActA1_004_16.txt",
-            ]
-        ],
-    )
-)
 
-# Map of image key to microns per pixel
-images = {
-    "34D-grid2-s3-acta1_001_16": 0.012723,
-    "34D-grid3-ActA1_007_16": 0.015904,
-    "34D-grid3-ActA1_013_16": 0.015904,
-    "34D-grid2-s2_002_16": 0.015904,
-    "34D-grid2-s5_005_16": 0.015904,
-    "34D-grid3-ActA1_020_16": 0.015904,
-    "34D-grid3-s6_005_16": 0.015904,
-    "34D-grid2-s3_028_16": 0.015904,
-    "34D-grid3-ActA1_001_16": 0.015904,
-    "34D-grid3-ActA1_002_16": 0.015904,
-    "34D-grid3-ActA1_003_16": 0.015904,
-    "34D-grid3-ActA1_004_16": 0.015904,
-}
-
-
+# Instantiate material properties
 parameters = {
     "Kb": 0.1 / 4,  # Bending modulus (pN um; original 1e-19 J)
     "Ksg": 50,  # Global stretching modulus (pN um/um^2; original 0.05 mN/m)
+    "Ksl": 1,
 }
-f_energy = partial(get_energy_2d_closed, **parameters)
-f_force = partial(get_force_2d_closed, **parameters)
-cm = mpl.cm.viridis_r
-target_edge_length = 0.05  # target edge length in um for resampling
+mem = ClosedPlaneCurveMaterial(**parameters)
 
-total_time = 0.01
+# Discretization settings
+target_edge_length = 0.05  # target edge length in um for resampling
+total_time = 0.1
 dt = 5e-6  # Timestep
 n_iter = math.floor(total_time / dt)  # Number of relaxation steps
 
+# Plotting settings
+padding = 2
+cm = mpl.cm.viridis_r
 
-def make_movie(file):
-    # energy_log = np.zeros(n_iter)
-    data = defaultdict(dict)
-    k = file.stem
 
+def make_movie(file, fps: int = 30, dpi: int = 100, skip: int = 100):
     original_coords = np.loadtxt(file)
     original_coords = np.vstack(
         (original_coords, original_coords[0])
@@ -102,89 +62,112 @@ def make_movie(file):
     n_vertices = math.floor(total_length / target_edge_length)
     # print(f"  Resampling to {n_vertices} vertices")
 
-    coords = np.zeros((n_iter + 1, n_vertices, 2))
-    forces = np.zeros((n_iter + 1, n_vertices, 2))
-
     # Periodic cubic B-spline interpolation with no smoothing (s=0)
     tck, _ = splprep([original_coords[:, 0], original_coords[:, 1]], s=0, per=True)
-    data[k]["spline"] = tck
 
     xi, yi = splev(np.linspace(0, 1, n_vertices), tck)
-    coords[0] = np.hstack((xi.reshape(-1, 1), yi.reshape(-1, 1)))
+    coords = np.hstack((xi.reshape(-1, 1), yi.reshape(-1, 1)))
 
-    # Perform energy relaxation
-    if n_iter > 0:
-        for i in tqdm(range(0, n_iter), desc="Energy relaxation"):
-            # energy, force = f_value_and_grad(relaxed_coords)
-            # energy_log[i] = f_energy(coords[i])
+    x_lim = np.array([np.min(coords[:, 0]), np.max(coords[:, 0])]) + [-padding, padding]
+    y_lim = np.array([np.min(coords[:, 1]), np.max(coords[:, 1])]) + [-padding, padding]
 
-            # Compute dual length
-            dc = np.roll(coords[i][:-1], -1, axis=0) - coords[i][:-1]
-            edgeLengths = np.linalg.norm(dc, axis=1)
-            dualLengths = ((edgeLengths + np.roll(edgeLengths, 1)) / 2.0).reshape(-1, 1)
-            dualLengths = np.vstack((dualLengths, dualLengths[0]))
-            forces[i] = -f_force(coords[i]) / dualLengths
+    c, e, f = fwd_euler_integrator(
+        coords, mem, n_steps=n_iter, dt=dt, save_trajectory=True
+    )
 
-            coords[i + 1] = np.array(coords[i] - f_force(coords[i]) * dt)
-            coords[i + 1][-1] = coords[i + 1][0]
-        # print(
-        #     f"  DELTA E: {energy_log[-1] - energy_log[0]}; E_before: {energy_log[0]}; E_after: {energy_log[-1]}"
-        # )
+    # print([i.shape for i in [c, e, f]])
 
-    rng = range(0, n_iter, 10)
-    with tempfile.TemporaryDirectory() as tmp_dir:
-        print(tmp_dir)
+    fig = plt.figure(figsize=(5, 5))
+    ax = fig.add_subplot(autoscale_on=False, xlim=x_lim, ylim=y_lim)
 
-        for i in tqdm(rng):
-            fig, ax = plt.subplots(1, 1, figsize=(5, 5))
+    (original_line,) = ax.plot(original_coords[:, 0], original_coords[:, 1], color="k")
 
-            ax.plot(coords[0][:, 0], coords[0][:, 1], color="k")
-            ax.plot(coords[i][:, 0], coords[i][:, 1], color="r")
+    (line,) = ax.plot(c[0][:, 0], c[0][:, 1], color="r")
+    forces = np.sum(f[0], axis=0)
+    f_mag = np.linalg.norm(forces, axis=1)
 
-            Q = ax.quiver(
-                coords[i][:, 0],
-                coords[i][:, 1],
-                forces[i][:, 0],
-                forces[i][:, 1],
-                np.linalg.norm(forces[i], axis=1),
-                cmap=cm,
-                angles="xy",
-                units="xy",
-                label="force",
-                scale=1,
-                scale_units="xy",
-                width=0.1,
-                zorder=10,
-                alpha=0.3,
+    q = ax.quiver(
+        c[0][:, 0],
+        c[0][:, 1],
+        forces[:, 0],
+        forces[:, 1],
+        f_mag,
+        cmap=mpl.cm.viridis_r,
+        angles="xy",
+        units="xy",
+        label="force",
+        scale=4e1,
+        scale_units="xy",
+        width=0.1,
+        zorder=10,
+    )
+
+    time_template = "Iteration = %d"
+    time_text = ax.text(0.05, 0.9, "", transform=ax.transAxes)
+
+    with Image.open(f"raw_images/{file.stem}.TIF") as im:
+        pixel_scale = images[file.stem]
+        x_lim_pix = (x_lim / pixel_scale).round()
+        y_lim_pix = (y_lim / pixel_scale).round()
+
+        im = im.crop((x_lim_pix[0], y_lim_pix[0], x_lim_pix[1], y_lim_pix[1]))
+
+        plt.imshow(
+            im,
+            alpha=0.6,
+            extent=(x_lim[0], x_lim[1], y_lim[1], y_lim[0]),
+            zorder=0,
+            cmap=plt.cm.Greys_r,
+        )
+
+    # Shrink current axis
+    box = ax.get_position()
+    ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+
+    # ax.legend(loc="center left", bbox_to_anchor=(1, 0.5))
+    cbar = fig.colorbar(
+        mpl.cm.ScalarMappable(
+            norm=mpl.colors.Normalize(vmin=f_mag.min(), vmax=f_mag.max()), cmap=cm
+        ),
+        ax=ax,
+    )
+    cbar.ax.get_yaxis().labelpad = 20
+    cbar.ax.set_ylabel("Force Density ($\mathregular{pN/\mu m^2}$)", rotation=270)
+
+    def animate(i):
+        line.set_data(c[i][:, 0], c[i][:, 1])
+        time_text.set_text(time_template % (i))
+
+        forces = np.sum(f[i], axis=0)
+        f_mag = np.linalg.norm(forces, axis=1)
+        q.set_UVC(forces[:, 0], forces[:, 1], f_mag)
+        q.set_offsets(c[i])
+
+        cbar.update_normal(
+            mpl.cm.ScalarMappable(
+                norm=mpl.colors.Normalize(vmin=f_mag.min(), vmax=f_mag.max()), cmap=cm
             )
+        )
 
-            ax.set_ylabel(r"X (μm)")
-            ax.set_xlabel(r"Y (μm)")
+        return line, time_text, q, cbar
 
-            # pixel_scale = images[k]
-            # x_lim = np.array(ax.get_xlim())
-            # y_lim = np.array(ax.get_ylim())
+    ## MAKE MOVIE
+    moviewriter = animation.FFMpegWriter(fps=fps)
+    with moviewriter.saving(fig, f"movies/{file.stem}.mp4", dpi=dpi):
+        for i in tqdm(np.arange(0, n_iter + 1, skip)):
+            animate(i)
+            moviewriter.grab_frame()
 
-            # x_lim_pix = (x_lim / pixel_scale).round()
-            # y_lim_pix = (y_lim / pixel_scale).round()
-
-            ax.set_ylim(ax.get_ylim()[::-1])
-
-            fig.savefig(f"{tmp_dir}/{file.stem}_{i}.png")
-            fig.clear()
-            plt.close(fig)
-
-        # clip = mpy.ImageSequenceClip(
-        #     [f"{tmp_dir}/{file.stem}_{i}.png" for i in rng],
-        #     fps=30,
-        # )
-        # clip.write_videofile(f"movies/{file.stem}_test_forces.mp4", fps=30)
-        # clip.close()
+    ## INTERACTIVE ANIMATION
+    # ani = animation.FuncAnimation(
+    #     fig, animate, np.arange(0, n_iter, 100), interval=200, blit=True
+    # )
+    # plt.show()
+    ax.clear()
+    plt.close(fig)
 
 
 if __name__ == "__main__":
-    j = []
-    for file in files:
-        j.append((file))
-
-    r = process_map(make_movie, j, max_workers=2, maxtasksperchild=1)
+    ## BATCH RENDER
+    f_movie = partial(make_movie, fps=30, dpi=200, skip=100)
+    r = process_map(f_movie, files, max_workers=8)
--- automembrane/integrator.py
+++ automembrane/integrator.py
@@ -4,9 +4,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-from functools import partial
-import jax
-
 import numpy as np
 import numpy.typing as npt
 from tqdm.auto import tqdm
@@ -14,12 +11,12 @@ from tqdm.auto import tqdm
 from .energy import Material
 
 
-# @partial(jax.jit, static_argnums=[1], static_argnames=["n_steps", "dt"])
 def fwd_euler_integrator(
     coords: npt.NDArray[np.float64],
     mat: Material,
     n_steps: int = int(1e5),
     dt: float = 5e-6,
+    save_trajectory: bool = False,
 ) -> tuple[npt.NDArray[np.float64], npt.NDArray[np.float64]]:
     """Perform forward Euler integration
 
@@ -28,18 +25,52 @@ def fwd_euler_integrator(
         mat (Material): Membrane material holding parameters
         n_steps (int, optional): Number of steps to take. Defaults to int(1e5).
         dt (float, optional): Time step. Defaults to 5e-6.
+        save_trajectory (bool, optional): Whether or not to save forces. Defaults to False.
 
     Returns:
-        tuple[npt.NDArray[np.float64], npt.NDArray[np.float64]]: _description_
+        Union[tuple[npt.NDArray[np.float64], npt.NDArray[np.float64]],tuple[npt.NDArray[np.float64], npt.NDArray[np.float64], npt.NDArray[np.float64]]]: Returns coordinates, and
     """
-    energy_log = np.zeros((n_steps, *mat._energy(coords).shape))
+    energy_shape = mat._energy(coords).shape
 
-    for i in tqdm(range(0, n_steps), desc="Energy relaxation"):
-        energy_log[i], force = mat._energy_force(coords)
-        # c_t+1 = c_t - force * dt
-        coords = np.array(coords - np.sum(force, axis=0) * dt)
-        coords[-1] = coords[0]
+    energy_log = np.zeros((n_steps + 1, *energy_shape))
+    if save_trajectory:
+        coord_log = np.zeros((n_steps + 1, *coords.shape))
+        force_log = np.zeros(
+            (
+                n_steps + 1,
+                *energy_shape,
+                *coords.shape,
+            )
+        )
+
+        coord_log[0] = coords
+
+        for i in tqdm(range(0, n_steps + 1), desc="Energy relaxation"):
+            # Compute dual length
+            delta_coords = np.roll(coords[:-1], -1, axis=0) - coords[:-1]
+            edgeLengths = np.linalg.norm(delta_coords, axis=1)
+            dualLengths = ((edgeLengths + np.roll(edgeLengths, 1)) / 2.0).reshape(-1, 1)
+            dualLengths = np.vstack((dualLengths, dualLengths[0]))
+
+            # Calculate energy and force
+            energy_log[i], force = mat._energy_force(coords)
+            force_log[i] = -force / dualLengths
+
+            # c_t+1 = c_t - force * dt
+            coords = np.array(coords - np.sum(force, axis=0) * dt)
+            coords[-1] = coords[0]
+            if i < n_steps:
+                coord_log[i + 1] = coords
+        return coord_log, energy_log, force_log
+
+    else:
+        for i in tqdm(range(0, n_steps), desc="Energy relaxation"):
+            energy_log[i], force = mat._energy_force(coords)
+            # c_t+1 = c_t - force * dt
+            coords = np.array(coords - np.sum(force, axis=0) * dt)
+            coords[-1] = coords[0]
+        return coords, energy_log
+
     # print(
     #     f"  DELTA E: {energy_log[-1] - energy_log[0]}; E_before: {energy_log[0]}; E_after: {energy_log[-1]}"
     # )
-    return coords, energy_log
