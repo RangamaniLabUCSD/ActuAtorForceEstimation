7646120e21b3ea19fe1662f1626cc5d8ba74f929
Cuncheng Zhu <cuzhu@ucsd.edu>
2022-08-15 13:05:47 -0700

    refactor: restructuring of the driver script

--- actuator_example/para_variation.py
+++ actuator_example/para_variation.py
@@ -36,26 +36,27 @@ cm = mpl.cm.viridis_r
 
 # Instantiate material properties
 parameters = {
-    "Kb": 0.1 / 4,  # Bending modulus (pN um; original 1e-19 J)
-    "Ksg": 50,  # Global stretching modulus (pN um/um^2; original 0.05 mN/m)
-    "Ksl": 1,
+    "Kb": 1 / 4,  # Bending modulus (pN um; original 1e-19 J)
+    "Ksg": 1,  # Global stretching modulus (pN um/um^2; original 0.05 mN/m)
+    "Ksl": 10,
 }
 mem = ClosedPlaneCurveMaterial(**parameters)
 
 # Discretization settings
 target_edge_length = 0.05  # target edge length in um for resampling
-total_time = 0.1
-dt = 5e-6  # Timestep
-n_iter = math.floor(total_time / dt)  # Number of relaxation steps
+# total_time = 0.001
+dt_ = 3e-5 # Timestep
+n_iter = 20000  # Number of relaxation steps
+# n_iter = math.floor(total_time / dt)  # Number of relaxation steps
 
 data = defaultdict(dict)
 
 def resample(original_coords, target_edge_length):
     total_length = np.sum(
-                np.linalg.norm(
-                    np.roll(original_coords[:-1], -1, axis=0) - original_coords[:-1], axis=1
-                )
-            )
+        np.linalg.norm(
+            np.roll(original_coords[:-1], -1, axis=0) - original_coords[:-1], axis=1
+        )
+    )
     n_vertices = math.floor(total_length / target_edge_length)
     print(f"  Resampling to {n_vertices} vertices")
     # Periodic cubic B-spline interpolation with no smoothing (s=0)
@@ -64,52 +65,72 @@ def resample(original_coords, target_edge_length):
     xi, yi = splev(np.linspace(0, 1, n_vertices), tck)
     coords = np.hstack((xi.reshape(-1, 1), yi.reshape(-1, 1)))
     return coords, tck
+
+
+def run(material, file, ifResample=False):
+    k = file.stem
+
+    print("Processing:", k)
+    original_coords = np.loadtxt(file)
+    original_coords = np.vstack(
+        (original_coords, original_coords[0])
+    )  # Energy expects last point to equal first
+
+    if ifResample:
+        coords, tck = resample(original_coords, target_edge_length)
+        data[k]["spline"] = tck
+    else:
+        coords = original_coords
+
+
+    # length_scale = np.sum(material.edge_length(coords)) / 2 / np.pi
+    curvature_scale = np.max(material.edge_curvature(coords))
+    original_coords = original_coords * curvature_scale
+    coords = coords * curvature_scale
+
+    # Perform energy relaxation
+    relaxed_coords = coords
+    dt = dt_ / parameters["Kb"] / curvature_scale**2
+    if n_iter > 0:
+        relaxed_coords, energy_log = fwd_euler_integrator(
+            relaxed_coords, material, n_steps=n_iter, dt=dt
+        )
+        print(
+            f"  DELTA E: {energy_log[-1] - energy_log[0]}; E_before: {energy_log[0]}; E_after: {energy_log[-1]}"
+        )
+
+    # Compute force density
+    relaxed_forces = sum(material.force(relaxed_coords)) / material.vertex_dual_length(
+        relaxed_coords
+    )
     
+    
+    np.save("foo.npy", relaxed_coords)
+    data[k]["original_coords"] = original_coords
+    data[k]["relaxed_coords"] = relaxed_coords
+    data[k]["relaxed_forces"] = relaxed_forces
 
-def run(material, files, ifResample = False):
-    for file in files:
-        k = file.stem
-        
-        print("Processing:", k)
-        original_coords = np.loadtxt(file)
-        original_coords = np.vstack(
-            (original_coords, original_coords[0])
-        )  # Energy expects last point to equal first
-        data[k]["original_coords"] = original_coords
+    return data, curvature_scale, material
 
-        if (ifResample):
-            coords, tck = resample(original_coords, target_edge_length)
-            data[k]["spline"] = tck
-        else: 
-            coords = original_coords
-            
-        length_scale = np.sum(material.edge_length(coords)) / 2/ np.pi
-        coords = coords / length_scale
-
-        # Perform energy relaxation
-        relaxed_coords = coords
-        if n_iter > 0:
-            relaxed_coords, energy_log = fwd_euler_integrator(
-                relaxed_coords, material, n_steps=n_iter, dt=dt
-            )
-            print(
-                f"  DELTA E: {energy_log[-1] - energy_log[0]}; E_before: {energy_log[0]}; E_after: {energy_log[-1]}"
-            )
-        data[k]["relaxed_coords"] = relaxed_coords
-
-        # Compute force density
-        relaxed_forces = sum(material.force(relaxed_coords)) / material.vertex_dual_length(relaxed_coords)
-        data[k]["relaxed_forces"] = relaxed_forces
-        
-        return data, length_scale, material
 
 def make_movie(
-    file_stem, original_coords, relaxed_coords, relaxed_forces, fps: int = 30, dpi: int = 100, skip: int = 100, interactive: bool = False
+    file_stem,
+    original_coords,
+    relaxed_coords,
+    relaxed_forces,
+    fps: int = 30,
+    dpi: int = 100,
+    skip: int = 100,
+    interactive: bool = False,
 ):
-    x_lim = np.array([np.min(original_coords[:, 0]), np.max(original_coords[:, 0])]) + [-padding, padding]
-    y_lim = np.array([np.min(original_coords[:, 1]), np.max(original_coords[:, 1])]) + [-padding, padding]
-
-    # print([i.shape for i in [c, e, f]])
+    x_lim = np.array([np.min(original_coords[:, 0]), np.max(original_coords[:, 0])]) + [
+        -padding,
+        padding
+    ]
+    y_lim = np.array([np.min(original_coords[:, 1]), np.max(original_coords[:, 1])]) + [
+        -padding,
+        padding,
+    ]
 
     fig = plt.figure(figsize=(5, 5))
     ax = fig.add_subplot(autoscale_on=False, xlim=x_lim, ylim=y_lim)
@@ -117,15 +138,14 @@ def make_movie(
     # flip y-axis
     ax.set_ylim(ax.get_ylim()[::-1])
 
-    (original_line,) = ax.plot(original_coords[:, 0], original_coords[:, 1], color="k")
+    # (original_line,) = ax.plot(original_coords[:, 0], original_coords[:, 1], color="k")
 
-    (line,) = ax.plot(relaxed_coords[0][:, 0], relaxed_coords[0][:, 1], color="r")
-    relaxed_forces = np.sum(relaxed_forces[0], axis=0)
+    (line,) = ax.plot(relaxed_coords[:, 0], relaxed_coords[:, 1], linewidth=0.2, color="r")
     f_mag = np.linalg.norm(relaxed_forces, axis=1)
 
     q = ax.quiver(
-        relaxed_coords[0][:, 0],
-        relaxed_coords[0][:, 1],
+        relaxed_coords[:, 0],
+        relaxed_coords[:, 1],
         relaxed_forces[:, 0],
         relaxed_forces[:, 1],
         f_mag,
@@ -170,44 +190,34 @@ def make_movie(
     )
     cbar.ax.get_yaxis().labelpad = 20
     cbar.ax.set_ylabel("Force Density ($\mathregular{pN/\mu m^2}$)", rotation=270)
-
-    def animate(i):
-        line.set_data(relaxed_coords[i][:, 0], relaxed_coords[i][:, 1])
-        time_text.set_text(time_template % (i))
-
-        forces = np.sum(forces[i], axis=0)
-        f_mag = np.linalg.norm(forces, axis=1)
-        cmap = mpl.cm.ScalarMappable(
-            norm=mpl.colors.Normalize(vmin=f_mag.min(), vmax=f_mag.max()), cmap=cm
-        )
-        cbar.update_normal(cmap)
-        q.set_UVC(forces[:, 0], forces[:, 1], f_mag)
-        q.set_offsets(relaxed_coords[i])
-        q.set_clim(f_mag.min(), f_mag.max())
-        return line, time_text, q, cbar
-
-    if interactive:
-        ## INTERACTIVE ANIMATION
-        ani = animation.FuncAnimation(
-            fig, animate, np.arange(0, n_iter, 100), interval=200, blit=True
-        )
-        plt.show()
-    else:
-        ## MAKE MOVIE
-        moviewriter = animation.FFMpegWriter(fps=fps)
-        with moviewriter.saving(fig, f"movies/{file_stem}.mp4", dpi=dpi):
-            for i in tqdm(np.arange(0, n_iter + 1, skip)):
-                animate(i)
-                moviewriter.grab_frame()
-    ax.clear()
-    plt.close(fig)
+    plt.show()
 
 
 if __name__ == "__main__":
     ## BATCH RENDER
     # f_movie = partial(make_movie, fps=30, dpi=200, skip=100)
     # r = process_map(f_movie, files, max_workers=6)
-    data, length_scale, material = run(mem, files, ifResample = True)
-    make_movie(
-    file_stem, original_coords, deformed_coords, forces, fps: int = 30, dpi: int = 100, skip: int = 100, interactive: bool = False
-)
+    from pathlib import Path
+    files = list(
+        map(
+            Path,
+            [
+                f"coordinates/{i}"
+                for i in [
+                    "cell1/34D-grid2-s3-acta1_001_16.txt",
+                ]
+            ],
+        )
+    )
+    for file in files:
+        data, curvature_scale, material = run(mem, file, ifResample=True)
+        print(curvature_scale)
+        make_movie(
+            file.stem,
+            data[file.stem]["original_coords"] / curvature_scale,
+            data[file.stem]["relaxed_coords"] / curvature_scale,
+            data[file.stem]["relaxed_forces"] * 0.1 * curvature_scale**3,
+            fps=30,
+            dpi=200,
+            skip=100,
+        )
